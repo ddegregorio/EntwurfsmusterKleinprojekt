021a48833642ed3ef250cc1b38a4f1bc09981f1f
Dominik De Gregorio <dominikdg@yahoo.de>
2017-11-22 16:02:22 +0100

    Adds Testclass for AServiceLayer

---
+++ test/ASL/AServiceLayerTest.java
@@ -0,0 +1,42 @@
+package ASL;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AServiceLayerTest {
+    AServiceLayer asl;
+
+    @BeforeEach
+    void setUp() {
+        // TODO create test admin user that is allowed to use the ASL
+
+        asl =  asl.getASLInstance("Sabine", "testitest");
+    }
+
+    @Test
+    void storeNewArticleInDB() {
+        if(asl != null){
+            asl.storeNewArticleInDB(5000, "articleTest", 5.8, false);
+
+        }
+        else{
+            assertFalse(true);
+        }
+    }
+
+    @Test
+    void deleteArticleInDB() {
+    }
+
+    @Test
+    void printAllArticlesFromDB() {
+    }
+
+    @Test
+    void setDiscountOnArticle() {
+
+    }
+
+}
